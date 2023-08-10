0f68d22dd5e62d621506085a7fd39e5dcc7fd346
JOONGHWANLEE <pppy11@naver.com>
2023-08-10 18:11:01 +0900

    이중환up

---
+++ src/main/java/com/travel/controller/PlannerController.java
@@ -0,0 +1,18 @@
+package com.travel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class PlannerController {
+
+	@GetMapping(value = "/planner")
+	public String main() {
+		return "planner/plannerMain";
+	}
+	
+	
+}
