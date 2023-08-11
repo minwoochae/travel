519fe9b9caf3ceca4d4d8300378f9b99ecfb0d06
minwoochae <mincw3@naver.com>
2023-08-11 11:02:55 +0900

    update

--- src/main/java/com/travel/controller/MemberController.java
+++ src/main/java/com/travel/controller/MemberController.java
@@ -3,36 +3,69 @@ package com.travel.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.travel.Dto.MemberFormDto;
+import com.travel.entity.Member;
 import com.travel.service.MemberService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 	
-	private final MemberService memberservice ;
+	private final MemberService memberservice;
+	private final PasswordEncoder passwordEncoder;
 	
 	//로그인 화면
 	@GetMapping(value = "/members/login")
 	public String loginmember() {
-		return "members/memberLoginForm";
+		return "member/memberLoginForm";
 	}
+
 	
 	//회원가입 화면
 	@GetMapping(value = "/members/new")
 	public String memberForme(Model model) {
 		model.addAttribute("memberFormDto", new MemberFormDto());
-		return "members/memberForm";
+		return "member/memberForm";
 	}
+	//회원가입
+	@PostMapping(value = "/members/new")
+	public String memberForm(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors()) {
+			return "member/memberForm";
+		}
+		
+		try {
+			Member member = Member.createMember(memberFormDto, passwordEncoder);
+			memberservice.saveMember(member);
+		} catch(IllegalStateException e) {
+			model.addAttribute("errorMessage", e.getMessage());
+			return "member/memberForm";
+		}
+		
+		return "redirect:/";
+	}
+	
+	
+	@GetMapping(value = "/members/login/error")
+	public String loginError(Model model) {
+		model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
+		
+		return "member/memberLoginForm";
+	}
+	
+	
 	
 	// 아이디 찾기
 	@GetMapping(value = "/account/search")
@@ -40,7 +73,6 @@ public class MemberController {
 		model.addAttribute("memberFormDto", new MemberFormDto());
 		return "members/LoginForm";
 	}
-
 	@PostMapping("/account/search")
 	@ResponseBody
 	public HashMap<String, String> members(@RequestBody Map<String, Object> data) {
