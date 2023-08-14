83b78ca6d6cb97e6699043f63c3ba18b754b8bd9
leejiheee <dlwlgml7212@naver.com>
2023-08-14 12:08:12 +0900

    jihee up

---
+++ src/main/java/com/travel/controller/ItemController.java
@@ -0,0 +1,21 @@
+package com.travel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+	
+	@GetMapping(value="/itemList")
+	public String itemList() {
+		return "/item/itemShopList";
+	}
+	
+	@GetMapping(value="/itemDtl")
+	public String itemDtl() {
+		return "/item/itemDtl";
+	}
+}
